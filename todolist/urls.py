3be48b2be6c06724fffad8875a8a1bf1bdc7f5d4
Swastik Adhikari <78269644+sskadk@users.noreply.github.com>
2025-06-11 09:21:32 +0545

    delete todo confirm

--- todolist/urls.py
+++ todolist/urls.py
@@ -6,8 +6,8 @@ from todo.views import *
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', home),
-    path('todo/', todo_view),
-    path('type/', type_view),
+    path('todo/', todo_view, name="todo_list"),
+    path('type/', type_view, name="type_list"),
     path('create_todo/', create_todo_view),
     path('create_type/', create_type_view),
     path('update_todo/<int:pk>/', update_todo_view, name="update-todo"),
@@ -15,3 +15,4 @@ urlpatterns = [
     path('update_type/<int:pk>/', update_type_view, name="update-type"),
     path('delete_type/<int:pk>/', delete_type_view, name="delete-type"),
 ]
+
